3fa1d52417ca3fc3cdd0ecc048d7e08cb24a1e94
= <ojeetdeol13@gmail.com>
2021-12-28 00:49:55 -0800

    gradient background

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function Message(){
-
+    return(
+        <div>
+            <input type = "text" id ="num_questions" size = "30" placeholder="Enter Number Of Questions Desired"/>
     
+    
+        </div>
+    )
 }
 
 ReactDOM.render(<Message/>, document.getElementById('root'))
