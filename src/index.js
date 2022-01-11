01a1d48c969af9fbe014cc0e65d575b1e0c97dc5
= <ojeetdeol13@gmail.com>
2022-01-10 20:56:31 -0800

    updated fetch_questions function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom'
 
 const url = "https://opentdb.com/api.php?amount="
 
-function fetch_questions(number_of_ques){
-    fetch()
+function fetch_questions(){
+    var number_of_ques = document.getElementById('input').value
+    if(!isNaN(number_of_ques)){
+        var temp_num = parseInt(number_of_ques)
+        fetch(url+number_of_ques)
+        .then(res=>res.json())
+        .then(data => console.log(data))
+    }
+    else{
+        console.log(1)
+        alert('You Did Not Enter A Number!')
+    }
+    
+
 
 }
 
@@ -16,8 +28,8 @@ function Message(){
             <h1 id = "top_logo_text">Trivia Mania</h1>
         </div>
         <div id ="num_questions">
-            <input type = "text"  size = "30" placeholder="Enter Number Of Questions Desired"/>
-            <button id = "submit_button">Enter</button>
+            <input id = 'input' type = "text"  size = "30" placeholder="Enter Number Of Questions Desired"/>
+            <button id = "submit_button" onClick = {fetch_questions}>Enter</button>
         </div>
         
             
